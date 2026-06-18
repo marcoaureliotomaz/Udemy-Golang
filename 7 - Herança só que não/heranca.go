1f149180bdb730e65c4e252a20089d0939486e64
marco <marcoaureliotomaz@gmail.com>
2026-06-18 15:06:42 -0300

    Herança

---
+++ 7 - Herança só que não/heranca.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+
+	p1 := pessoa{"João", "Pedro", 19, 170}
+
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "CC", "UFERSA"}
+	fmt.Println(e1)
+	fmt.Println(e1.nome)
+	fmt.Println(e1.altura)
+}
