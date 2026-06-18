6835fdf13f9bc49b6618e113915961233137fcfe
marco <marcoaureliotomaz@gmail.com>
2026-06-18 17:01:50 -0300

    Slices e make

--- 8 -  Arrays e Slices/arrays-e-slices.go
+++ 8 -  Arrays e Slices/arrays-e-slices.go
@@ -35,4 +35,31 @@ func main() {
 
 	array2[1] = "Pocição alterada"
 	fmt.Println(slice2)
+
+	//Arrays Internos
+
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice3 = append(slice3, 5)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice3 = append(slice3, 50)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3))
+	fmt.Println(cap(slice3))
+
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
+
+	slice4 = append(slice4, 50)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4))
+	fmt.Println(cap(slice4))
 }
